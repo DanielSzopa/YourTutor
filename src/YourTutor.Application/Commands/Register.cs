8c37d50fdd29e86df2308cf3eb4ea2bad4b1006f
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-03-03 22:48:31 +0100

    Change property to use init instead of set

--- src/YourTutor.Application/Commands/Register.cs
+++ src/YourTutor.Application/Commands/Register.cs
@@ -7,22 +7,22 @@ public class Register : IRequest<Unit>
 {
     [Required(ErrorMessage = "Email is required")]
     [EmailAddress(ErrorMessage = "Email address is invalid")]
-    public string Email { get; set; }
+    public string Email { get; init; }
 
     [Required(ErrorMessage = "First name is required")]
     [StringLength(50, MinimumLength = 2, ErrorMessage = "Must be between 2 and 50 characters")]
-    public string FirstName { get; set; }
+    public string FirstName { get; init; }
 
     [Required(ErrorMessage = "Last name is required")]
     [StringLength(50, MinimumLength = 2, ErrorMessage = "Must be between 2 and 50 characters")]
-    public string LastName { get; set; }
+    public string LastName { get; init; }
 
     [Required(ErrorMessage = "Password is required")]
     [StringLength(50, MinimumLength = 8, ErrorMessage = "Must be between 2 and 50 characters")]
-    public string Password { get; set; }
+    public string Password { get; init; }
 
     [Required(ErrorMessage = "Password confirmation is required")]
     [Compare(nameof(Register.Password), ErrorMessage = "Passwords must be the same")]
-    public string PasswordConfirmation { get; set; }
+    public string PasswordConfirmation { get; init; }
 
 }
