85018e402f7daa5422d09d7d56082b1a9d89f5aa
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-03-03 23:38:25 +0100

    Add httpContextAbstraction

--- src/YourTutor.Application/Commands/Handlers/RegisterHandler.cs
+++ src/YourTutor.Application/Commands/Handlers/RegisterHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using YourTutor.Core.Abstractions.Repositories;
 using YourTutor.Core.Entities;
 using YourTutor.Core.ValueObjects;
@@ -8,10 +9,12 @@ namespace YourTutor.Application.Commands.Handlers
     public class RegisterHandler : IRequestHandler<Register, Unit>
     {
         private readonly IUserRepository _userRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public RegisterHandler(IUserRepository userRepository)
+        public RegisterHandler(IUserRepository userRepository, IHttpContextAccessor httpContextAccessor)
         {
             _userRepository = userRepository;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<Unit> Handle(Register command, CancellationToken cancellationToken)
