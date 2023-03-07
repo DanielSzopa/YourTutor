e6849a6ed6fcb2f9b32f5cf1d6de7041a0a93096
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-03-07 19:45:50 +0100

    Add expiresDays settings for identity cookie

--- src/YourTutor.Application/Settings/IdentitySettings.cs
+++ src/YourTutor.Application/Settings/IdentitySettings.cs
@@ -8,12 +8,17 @@ namespace YourTutor.Infrastructure.Settings
         public static string SectionName => "Identity";
 
         public string CookieName { get; set; }
+        public int ExpiresDays { get; set; }
 
         public IdentitySettings()
         {
            RuleFor(x => x.CookieName)
                 .NotEmpty()
                 .WithMessage($"{nameof(CookieName)} can not be empty");
+
+            RuleFor(x => x.ExpiresDays)
+               .GreaterThan(0)
+               .WithMessage($"{nameof(ExpiresDays)} can not be less than 0");
         }       
     }
 }
--- src/YourTutor.Infrastructure/Services/SignInManager.cs
+++ src/YourTutor.Infrastructure/Services/SignInManager.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using System.Security.Claims;
 using YourTutor.Core.Abstractions;
 using YourTutor.Infrastructure.Constans;
+using YourTutor.Infrastructure.Settings;
 
 namespace YourTutor.Core.Services.SignInManager
 {
     internal sealed class SignInManager : ISignInManager
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IdentitySettings _identitySettings;
 
-        public SignInManager(IHttpContextAccessor httpContextAccessor)
+        public SignInManager(IHttpContextAccessor httpContextAccessor, IOptions<IdentitySettings> identitySettings)
         {
             _httpContextAccessor = httpContextAccessor;
+            _identitySettings = identitySettings.Value;
         }
 
         public async Task SignInAsync(bool isPersistent, Guid userId, string fullName)
@@ -28,7 +32,7 @@ namespace YourTutor.Core.Services.SignInManager
             var authProps = new AuthenticationProperties()
             {
                 IsPersistent = isPersistent,
-                ExpiresUtc = DateTimeOffset.UtcNow.AddDays(10),
+                ExpiresUtc = DateTimeOffset.UtcNow.AddDays(_identitySettings.ExpiresDays),
             };
 
             await _httpContextAccessor.HttpContext.SignInAsync(Schemes.IdentityScheme, claimIdentity, authProps);
