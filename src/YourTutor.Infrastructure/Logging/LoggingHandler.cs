0af909d0fdff324129be52d4a51f0a24e41b74d6
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-03-09 09:05:46 +0100

    Basic implementation for logging Handler

---
+++ src/YourTutor.Infrastructure/Logging/LoggingHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using YourTutor.Core.Abstractions;
+
+namespace YourTutor.Infrastructure.Logging
+{
+    public class LoggingHandler<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingHandler<TRequest, TResponse>> _logger;
+        private readonly IClock _clock;
+
+        public LoggingHandler(ILogger<LoggingHandler<TRequest, TResponse>> logger, IClock clock)
+        {
+            _logger = logger;
+            _clock = clock;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting request {@RequestName}, {@DateTime}", typeof(TRequest).Name, _clock.Now);
+
+            var result = await next();
+
+            _logger.LogInformation("Completed request {@RequestName}, {@DateTime}", typeof(TRequest).Name, _clock.Now);
+
+            return result;
+        }
+    }
+}
+
+
---
+++ src/YourTutor.Infrastructure/Services/Clock.cs
@@ -0,0 +1,11 @@
+﻿using YourTutor.Core.Abstractions;
+
+namespace YourTutor.Infrastructure.Services
+{
+    public class Clock : IClock
+    {
+        public DateTime Now => DateTime.UtcNow;
+    }
+}
+
+
