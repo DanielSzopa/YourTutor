8e00db876d3458bfc5a016abbdd011b01bb3508f
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-03-09 21:35:16 +0100

    Add logging to databaseInitializer

--- src/YourTutor.Infrastructure/DAL/DatabaseInitializer.cs
+++ src/YourTutor.Infrastructure/DAL/DatabaseInitializer.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using YourTutor.Infrastructure.Logging;
 
 namespace YourTutor.Infrastructure.DAL;
 
 internal sealed class DatabaseInitializer : IHostedService
 {
     private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<DatabaseInitializer> _logger;
 
     public DatabaseInitializer(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
+        _logger = _serviceProvider.GetLogger<DatabaseInitializer>();
     }
     public async Task StartAsync(CancellationToken cancellationToken)
     {
@@ -19,10 +23,16 @@ internal sealed class DatabaseInitializer : IHostedService
 
         if (dbContext.Database.IsRelational())
         {
+            _logger.LogInformation("Check migrations...");
             var pendingMigrations = await dbContext.Database.GetPendingMigrationsAsync();
             if (pendingMigrations != null && pendingMigrations.Any())
             {
                 await dbContext.Database.MigrateAsync();
+                _logger.LogInformation("Migrations have been added to database");
+            }
+            else
+            {
+                _logger.LogInformation("Database is up to date, do not need add any migrations");
             }
         }
     }
---
+++ src/YourTutor.Infrastructure/Logging/LoggerExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace YourTutor.Infrastructure.Logging
+{
+    internal static class LoggerExtension
+    {
+        internal static ILogger<T> GetLogger<T>(this IServiceProvider serviceProvider)
+            where T : class 
+            => serviceProvider.GetRequiredService<ILogger<T>>();
+    }
+}
+
+
