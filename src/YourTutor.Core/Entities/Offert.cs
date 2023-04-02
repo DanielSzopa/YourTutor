0ce5b36590ab1179f11b1e01ae1dd0aa6220ab97
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-04-02 10:48:50 +0200

    Add offertId value object

--- src/YourTutor.Core/Entities/Offert.cs
+++ src/YourTutor.Core/Entities/Offert.cs
@@ -1,34 +1,33 @@
 ﻿using YourTutor.Core.ValueObjects;
 
-namespace YourTutor.Core.Entities
+namespace YourTutor.Core.Entities;
+
+public sealed class Offert
 {
-    public sealed class Offert
+    public OffertId Id { get; private set; }
+
+    public Offert(OffertId id, string description, string subject, int priceFrom,
+        int priceTo, bool isRemotely, string location, Tutor tutor, UserId tutorId)
     {
-        public Guid Id { get; private set; }
-
-        public Offert(Guid id, string description, string subject, int priceFrom,
-            int priceTo, bool isRemotely, string location, Tutor tutor, UserId tutorId)
-        {
-            Id = id;
-            Description = description;
-            Subject = subject;
-            PriceFrom = priceFrom;
-            PriceTo = priceTo;
-            IsRemotely = isRemotely;
-            Location = location;
-            Tutor = tutor;
-            TutorId = tutorId;
-        }
-
-        public string Description { get; private set; }
-        public string Subject { get; private set; }
-        public int PriceFrom { get; private set; }
-        public int PriceTo { get; private set; }
-        public bool IsRemotely { get; private set; }
-        public string Location { get; private set; }
-        public Tutor Tutor { get; private set; }
-        public UserId TutorId { get; private set; }
+        Id = id;
+        Description = description;
+        Subject = subject;
+        PriceFrom = priceFrom;
+        PriceTo = priceTo;
+        IsRemotely = isRemotely;
+        Location = location;
+        Tutor = tutor;
+        TutorId = tutorId;
     }
+
+    public string Description { get; private set; }
+    public string Subject { get; private set; }
+    public int PriceFrom { get; private set; }
+    public int PriceTo { get; private set; }
+    public bool IsRemotely { get; private set; }
+    public string Location { get; private set; }
+    public Tutor Tutor { get; private set; }
+    public UserId TutorId { get; private set; }
 }
 
 
---
+++ src/YourTutor.Core/Exceptions/InvalidOffertIdException.cs
@@ -0,0 +1,12 @@
+﻿namespace YourTutor.Core.Exceptions
+{
+    internal sealed class InvalidOffertIdException : CustomException
+    {
+        internal InvalidOffertIdException(Guid value) : base($"Invalid offertId, value: {value}")
+        {
+            
+        }
+    }
+}
+
+
