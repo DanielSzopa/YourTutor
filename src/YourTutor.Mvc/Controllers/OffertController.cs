ac71e7a5c2e219404264399f5f19f3b6e2ff6f1a
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-04-02 11:09:28 +0200

    Create offert controller and Create view

---
+++ src/YourTutor.Mvc/Controllers/OffertController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YourTutor.Mvc.Controllers
+{
+    [Route("[controller]")]
+    public class OffertController : Controller
+    {
+        [Authorize]
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
