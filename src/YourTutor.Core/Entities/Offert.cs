fe98b0f62cab9cc902e045aebb062af25014004e
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-04-02 10:42:32 +0200

    Add basic offert entity

---
+++ src/YourTutor.Core/Entities/Offert.cs
@@ -0,0 +1,34 @@
+﻿using YourTutor.Core.ValueObjects;
+
+namespace YourTutor.Core.Entities
+{
+    public sealed class Offert
+    {
+        public Guid Id { get; private set; }
+
+        public Offert(Guid id, string description, string subject, int priceFrom,
+            int priceTo, bool isRemotely, string location, Tutor tutor, UserId tutorId)
+        {
+            Id = id;
+            Description = description;
+            Subject = subject;
+            PriceFrom = priceFrom;
+            PriceTo = priceTo;
+            IsRemotely = isRemotely;
+            Location = location;
+            Tutor = tutor;
+            TutorId = tutorId;
+        }
+
+        public string Description { get; private set; }
+        public string Subject { get; private set; }
+        public int PriceFrom { get; private set; }
+        public int PriceTo { get; private set; }
+        public bool IsRemotely { get; private set; }
+        public string Location { get; private set; }
+        public Tutor Tutor { get; private set; }
+        public UserId TutorId { get; private set; }
+    }
+}
+
+
