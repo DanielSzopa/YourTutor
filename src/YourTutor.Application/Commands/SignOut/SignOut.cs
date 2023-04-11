7f17dcb5f306aa2369611957a3674b6d86e30f99
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-04-11 09:55:15 +0200

    Change SignOut to record

--- src/YourTutor.Application/Commands/SignOut/SignOut.cs
+++ src/YourTutor.Application/Commands/SignOut/SignOut.cs
@@ -1,10 +1,7 @@
 ﻿using MediatR;
 
-namespace YourTutor.Application.Commands.SignOut
-{
-    public class SignOut : IRequest<Unit>
-    {
-    }
-}
+namespace YourTutor.Application.Commands.SignOut;
+
+public sealed record SignOut : IRequest<Unit>;
 
 
