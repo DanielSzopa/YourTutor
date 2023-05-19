e7aca63f4121c9e4cef39a97f3d3380460d7e45f
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-05-19 09:53:26 +0200

    Init setup for integration tests

---
+++ YourTutor.Tests.Integration/YourTutorApp.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace YourTutor.Tests.Integration;
+
+internal sealed class YourTutorApp : WebApplicationFactory<Program>
+{
+    public HttpClient Client { get; }
+    public YourTutorApp()
+    {
+        Client = WithWebHostBuilder(builder =>
+        {
+
+        })
+            .CreateClient();
+    }
+}
+
+
