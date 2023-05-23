a86423ae18f6b44a887ea670687e4023cff35d6e
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-05-23 11:21:18 +0200

    Reduce testCases for Register endpoint

--- YourTutor.Tests.Integration/Controllers/AccountControllerTests.cs
+++ YourTutor.Tests.Integration/Controllers/AccountControllerTests.cs
@@ -4,6 +4,7 @@ using System.Net;
 using YourTutor.Application.Abstractions.Security;
 using YourTutor.Application.Settings;
 using YourTutor.Application.ViewModels;
+using YourTutor.Core.ValueObjects;
 using YourTutor.Infrastructure.DAL;
 using YourTutor.Tests.Integration.Helpers;
 using YourTutor.Tests.Integration.TestFactories;
@@ -47,48 +48,39 @@ public class AccountControllerTests : IAsyncLifetime
         PasswordConfirmation = "Test123!",
     };
 
+    #region Register
+
     [Fact]
-    public async Task Register_WithValidVm_Should_AddUserToDb()
+    public async Task Register_WithValidVm_Should_AddUserToDbWithHashedPassword_And_Return302Found_And_SetLocationHeader_And_SetIdentityCookie()
     {
         //arrange
         var formContent = _validRegisterVm.ToFormContent();
 
         //act
-        await _client.PostAsync(_registerEndpoint, formContent);
+        var response = await _client.PostAsync(_registerEndpoint, formContent);
 
         //assert
         var user = await _db.Users
             .Include(u => u.Tutor)
             .FirstOrDefaultAsync();
+        var verifyResult = GetHashService().VerifyPassword(_validRegisterVm.Password, user.HashPassword);
 
         using var scope = new AssertionScope();
         user.Should().NotBeNull();
         user.Email.Value.Should().Be(_validRegisterVm.Email);
         user.FirstName.Value.Should().Be(_validRegisterVm.FirstName);
         user.LastName.Value.Should().Be(_validRegisterVm.LastName);
-        user.HashPassword.Value.Should().NotBeEmpty();
         user.Id.Value.Should().NotBe(Guid.Empty);
         user.Tutor.Should().NotBeNull();
-    }
+        verifyResult.Should().BeTrue();
 
-    [Fact]
-    public async Task Register_WithValidVm_Should_Return302Found_SetLocationHeader_SetIdentityCookie()
-    {
-        //arrange
-        var formContent = _validRegisterVm.ToFormContent();
-
-        //act
-        var response = await _client.PostAsync(_registerEndpoint, formContent);
-
-        //assert
-        using var scope = new AssertionScope();
         response.StatusCode.Should().Be(HttpStatusCode.Found);
         response.Headers.Location?.OriginalString.Should().Be("/");
         response.ContainsCookie(_identityCookie).Should().BeTrue();
     }
 
     [Fact]
-    public async Task Register_WithInvalidVm_Should_Return200Ok_WithoutLocationHeader_WithoutIdentityCookie()
+    public async Task Register_WithInvalidVm_Should_DoNotAddUserToDb_And_Return200Ok_And_WithoutSetLocationHeader_And_WithoutIdentityCookie()
     {
         //arrange
         var formContent = _invalidRegisterVm.ToFormContent();
@@ -97,25 +89,13 @@ public class AccountControllerTests : IAsyncLifetime
         var response = await _client.PostAsync(_registerEndpoint, formContent);
 
         //assert
+        var result = await _db.Users.AnyAsync();
+      
         using var scope = new AssertionScope();
+        result.Should().BeFalse();
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         response.Headers.Location.Should().BeNull();
         response.ContainsCookie(_identityCookie).Should().BeFalse();
-    }
-
-    [Fact]
-    public async Task Register_WithInvalidVm_Should_NotAddUserToDb()
-    {
-        //arrange
-        var formContent = _invalidRegisterVm.ToFormContent();
-
-        //act
-        await _client.PostAsync(_registerEndpoint, formContent);
-
-        //assert
-        var result = await _db.Users.AnyAsync();
-
-        result.Should().BeFalse();
     }
 
     [Fact]
@@ -138,7 +118,7 @@ public class AccountControllerTests : IAsyncLifetime
     }
 
     [Fact]
-    public async Task Register_WhenEmailIsAlreadyExist_ShouldNotAddUserToDb()
+    public async Task Register_WhenEmailIsAlreadyExist_ShouldNotAddUserToDb_Return200Ok_And_WithoutSetLocationHeader_And_WithoutIdentityCookie()
     {
         //arrange
         var user = TestUserFactory.User;
@@ -152,17 +132,24 @@ public class AccountControllerTests : IAsyncLifetime
         };
 
         await _db.Users.AddAsync(user);
+        await _db.SaveChangesAsync();
 
         var formContent = vm.ToFormContent();
 
         //act
-        await _client.PostAsync(_registerEndpoint, formContent);
+        var response = await _client.PostAsync(_registerEndpoint, formContent);
 
         //assert
         var result = await _db.Users.CountAsync(x => x.Email == user.Email);
 
+        using var scope = new AssertionScope();
         result.Should().Be(1);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        response.Headers.Location.Should().BeNull();
+        response.ContainsCookie(_identityCookie).Should().BeFalse();
     }
+
+    #endregion
 
 
     private IHashService GetHashService()
