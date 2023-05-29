ee47cf696aeba116274eaabd60580e1771a438bd
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-05-29 18:10:40 +0200

    Add azure sql db creating step

--- tests/YourTutor.Tests.Integration/Setup/TestDatabase.cs
+++ tests/YourTutor.Tests.Integration/Setup/TestDatabase.cs
@@ -1,12 +1,15 @@
-﻿using YourTutor.Infrastructure.DAL;
+﻿using Microsoft.Data.SqlClient;
+using YourTutor.Infrastructure.DAL;
 
 namespace YourTutor.Tests.Integration.Setup;
 
 internal class TestDatabase
 {
     internal YourTutorDbContext YourTutorDbContext { get; }
+    private readonly string _connectionString;
     internal TestDatabase(string connectionString)
     {
+        _connectionString = connectionString;
         var builder = new DbContextOptionsBuilder<YourTutorDbContext>();
         var options = builder.UseSqlServer(connectionString,
             x => x.MigrationsHistoryTable(ConstantsDAL.MigrationsHistoryTable, ConstantsDAL.DefaultSchema))
@@ -16,6 +19,22 @@ internal class TestDatabase
 
     internal async Task InitializeDbAsync()
     {
+        if (!await YourTutorDbContext.Database.CanConnectAsync())
+        {
+            SqlConnectionStringBuilder connstrBldr = new SqlConnectionStringBuilder(_connectionString);
+            connstrBldr.InitialCatalog = "master";
+
+            using (SqlConnection conn = new(connstrBldr.ConnectionString))
+            {
+                await conn.OpenAsync();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandTimeout = 120;
+                cmd.CommandText = "CREATE DATABASE [" + YourTutorDbContext.Database.GetDbConnection().Database + "] (EDITION = 'Standard')";
+                await cmd.ExecuteNonQueryAsync();
+                await conn.CloseAsync();
+            }
+        }
+
         var pendingMigrations = await YourTutorDbContext.Database.GetPendingMigrationsAsync();
         if (pendingMigrations != null && pendingMigrations.Any())
         {
