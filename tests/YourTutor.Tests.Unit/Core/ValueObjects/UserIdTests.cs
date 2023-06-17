01edd75b27dc136ef2c875822b1e404cae4532a5
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-06-17 13:12:08 +0200

    Add VO UserId tests

---
+++ tests/YourTutor.Tests.Unit/Core/ValueObjects/UserIdTests.cs
@@ -0,0 +1,47 @@
+﻿using YourTutor.Core.Exceptions;
+using YourTutor.Core.ValueObjects;
+
+namespace YourTutor.Tests.Unit.Core.ValueObjects;
+
+public class UserIdTests
+{
+    [Fact]
+    public void UserId_WhenValueIsGuidEmpty_ShouldThrowInvalidUserIdException()
+    {
+        try
+        {
+            //act
+            var userId = new UserId(Guid.Empty);
+        }
+        catch (InvalidUserIdException ex)
+        {
+            //assert
+            Assert.True(true);
+            return;
+        }
+
+        //assert
+        Assert.True(false);
+    }
+
+    [Fact]
+    public void OfferId_WhenValueIsValid_ShouldNotThrowInvalidUserIdException()
+    {
+        try
+        {
+            //act
+            var userId = new UserId(Guid.NewGuid());
+        }
+        catch (InvalidUserIdException ex)
+        {
+            //assert
+            Assert.True(false);
+            return;
+        }
+
+        //assert
+        Assert.True(true);
+    }
+}
+
+
