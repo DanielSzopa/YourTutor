248198e356805f1b9b143b8b04b006f742fdbc30
daniel.szopa333 <daniel.szopa333@gmail.com>
2023-06-17 13:08:14 +0200

    Add VO OfferId tests

---
+++ tests/YourTutor.Tests.Unit/Core/ValueObjects/OffertIdTests.cs
@@ -0,0 +1,54 @@
+﻿using YourTutor.Core.Exceptions;
+using YourTutor.Core.ValueObjects;
+using YourTutor.Tests.Unit.Fixtures;
+
+namespace YourTutor.Tests.Unit.Core.ValueObjects;
+
+public class OfferIdTests : IClassFixture<FakerFixture>
+{
+    private readonly Faker _faker;
+    public OfferIdTests(FakerFixture fakerFixture)
+    {
+        _faker = fakerFixture.Faker;
+    }
+
+    [Fact]
+    public void OfferId_WhenValueIsGuidEmpty_ShouldThrowInvalidOfferIdException()
+    {
+        try
+        {
+            //act
+            var offerId = new OfferId(Guid.Empty);
+        }
+        catch (InvalidOfferIdException ex)
+        {
+            //assert
+            Assert.True(true);
+            return;
+        }
+
+        //assert
+        Assert.True(false);
+    }
+
+    [Fact]
+    public void OfferId_WhenValueIsValid_ShouldNotThrowInvalidOfferIdException()
+    {
+        try
+        {
+            //act
+            var offerId = new OfferId(Guid.NewGuid());
+        }
+        catch (InvalidOfferIdException ex)
+        {
+            //assert
+            Assert.True(false);
+            return;
+        }
+
+        //assert
+        Assert.True(true);
+    }
+}
+
+
